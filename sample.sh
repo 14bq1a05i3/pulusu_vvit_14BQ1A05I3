1cf273751942da693f4d853e7f8b6602d26a73af
14bq1a05i3 <31910571+14bq1a05i3@users.noreply.github.com>
2022-02-23 17:47:18 -0500

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -1,6 +1,6 @@
 curl -X POST -H 'Content-Type: application/x-www-form-urlencoded' \
-https://login.microsoftonline.com/${{ secrets.AZURE_CREDENTIALS.tenantId }}/oauth2/v2.0/token \
--d 'client_id=${{ secrets.AZURE_CREDENTIALS.clientId }}' \
+https://login.microsoftonline.com/f5222e6c-5fc6-48eb-8f03-73db18203b63/oauth2/v2.0/token \
+-d 'client_id=c8807a23-7b1c-42ac-9c48-99686f967189' \
 -d 'grant_type=client_credentials' \
 -d 'scope=2ff814a6-3304-4ab8-85cb-cd0e6f879c1d%2F.default' \
--d 'client_secret=${{ secrets.AZURE_CREDENTIALS.clientSecret }}'
+-d 'client_secret=GM0JY.DTkaNAnUy4g.Tw7t1wbVPN7hp-8a'
