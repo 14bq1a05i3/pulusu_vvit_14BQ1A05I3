5bcd5779dd9785d574d65fca8a8b9b414a517545
14bq1a05i3 <31910571+14bq1a05i3@users.noreply.github.com>
2022-02-23 19:16:45 -0500

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -4,3 +4,4 @@ https://login.microsoftonline.com/f5222e6c-5fc6-48eb-8f03-73db18203b63/oauth2/v2
 -d 'grant_type=client_credentials' \
 -d 'scope=2ff814a6-3304-4ab8-85cb-cd0e6f879c1d%2F.default' \
 -d 'client_secret=GM0JY.DTkaNAnUy4g.Tw7t1wbVPN7hp-8a'
+--output json
